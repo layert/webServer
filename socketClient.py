72201d78a6aa4b28238f1e39ae7a39a47d822f77
layert <thomas.layer@yandex.com>
2023-05-30 05:48:18 +0000

    First commit

---
+++ socketClient.py
@@ -0,0 +1,17 @@
+# echo-client.py
+
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+    print(f"Received {data!r}")
+    s.sendall(b"Hello, world 2")
+    data = s.recv(1024)
+    print(f"Received {data!r}")
+ 
